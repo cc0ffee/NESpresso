800652fae684396694ffe204bb7d9bab427917a8
cc0ffee <stasikkacper1@gmail.com>
2026-09-05 15:12:43 -0500

    fix(mapper): scale selected PRG bank by its byte size

--- src/mappers/mapper2.cpp
+++ src/mappers/mapper2.cpp
@@ -3,16 +3,16 @@
 Mapper2::Mapper2(std::uint8_t prg_banks, std::uint8_t chr_banks) : Mapper(prg_banks, chr_banks) {}
 
 std::optional<std::uint32_t> Mapper2::cpu_memRead(std::uint16_t addr) const {
-
+    
     if (addr < 0x8000) {
         return std::nullopt;
     }
 
     if (addr < 0xC000) {
-        return ((prg_bank_ % prg_banks_) + (addr & 0x3FFF));
+        return ((prg_bank_ % prg_banks_) * 0x4000u + (addr & 0x3FFF));
     }
 
-    return ((prg_banks_ - 1) * 0x4000u) + (addr & 0x3FFF);
+    return ((prg_banks_ - 1) * 0x4000u) + (addr & 0x3FFF); 
 }
 
 void Mapper2::cpu_memWrite(std::uint16_t addr, std::uint8_t val) {
